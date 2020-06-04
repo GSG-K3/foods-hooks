1791702300255f6ae34c93aaee6dabe41fb6318e
tasneembhiri <141059@ppu.edu.ps>
2020-06-04 16:01:38 +0300

    final home page#3

--- src/Layout/BaseLayout.jsx
+++ src/Layout/BaseLayout.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from '../Components/Header';
-import Home from './Home';
-import One from '../Components/One';
-import Two from '../Components/Two';
-import Three from '../Components/Three';
+import Home from './Home/index.jsx';
 export default () => (
   <React.Fragment>
     <Router>
@@ -17,9 +14,7 @@ export default () => (
           <Grid sx={0} sm={3} />
           <Grid sx={12} sm={9}>
             <Switch>
-              <Route exact path="/" component={One} />
-              <Route exact path="/category" component={Two} />
-              <Route exact path="/meals-by-area" component={Three} />
+              <Route exact path="/" component={Home} />
             </Switch>
           </Grid>
           <Grid sx={0} sm={3} />
